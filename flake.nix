a956c7ada5e1374db46a453c8e3ed5acb62c612c
NJ <226465585+nj-twice@users.noreply.github.com>
2026-05-09 17:17:19 +0200

    Add CSS language server

--- flake.nix
+++ flake.nix
@@ -8,9 +8,12 @@
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
     packages.x86_64-linux.default = pkgs.callPackage ./nix/site.nix { inherit self; };
-    devShells.default = pkgs.mkShell {
+    devShells.x86_64-linux.default = pkgs.mkShell {
       inputsFrom = [
         self.packages.x86_64-linux.default
+      ];
+      packages = with pkgs; [
+        vscode-css-languageserver
       ];
     };
   };
